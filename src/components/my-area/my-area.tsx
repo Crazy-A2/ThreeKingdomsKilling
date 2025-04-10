144b421d675a89a892fff2cc5932e4ffbd67bc0d
Marial <68056912+Crazy-A2@users.noreply.github.com>
2025-04-10 13:14:45 +0800

    我的区域组件调整

--- src/components/my-area/my-area.tsx
+++ src/components/my-area/my-area.tsx
@@ -1,80 +1,99 @@
-import { component$, useStore } from '@builder.io/qwik';
-// import type { General as WuJiang } from '../../data/wujiang-junzheng-biaozhun'
+import { component$, useStore } from '@builder.io/qwik'
+import type { Skill } from '../../data/wujiang-junzheng-biaozhun'
 import { SkillType, TriggerTiming } from '../../data/wujiang-junzheng-biaozhun'
 // import type { Hand as ShouPai } from '../../data/shoupai-junzheng'
 import type { Player } from '../../utils/player'
 import { General } from '../general/general'
+// import { playerContext } from '../../routes/index'
+
+import { shoupaiArray } from '../../data/shoupai-junzheng'
+import { Hand } from '../hand/hand'
 
 export interface MyAreaProps {
-  // general: WuJiang // 武将
-  // handList: ShouPai[] // 手牌列表
-  player: Player // 玩家对象
+    player: Player // 玩家对象
 }
 
 /** 我的区域（真人玩家操控）*/
 export const MyArea = component$<MyAreaProps>((props) => {
-  return (
-    <div style={{
-      display: 'flex', width: '100%', flexDirection: 'row-reverse', position: 'fixed', bottom: 0
-    }}>
-      <General wujiang={props.player.general} />
+    // const player = useContext(playerContext)
+    return (
+        <div style={{ display: 'flex', width: '100%', position: 'fixed', bottom: 0 }}>
+            <General wujiang={props.player.general} />
 
-      <SkillArea />
+            <SkillArea skillList={props.player.skillList}/>
 
-      <HandArea />
-    </div>
-  )
+            <HandArea />
+        </div>
+    )
 })
 
-interface SkillAreaProps { }
+interface SkillAreaProps { 
+    skillList: Skill[] // 技能列表
+}
 
 /** 技能区 */
-const SkillArea = component$<SkillAreaProps>(() => {
-  // 测试
-  const skillList = useStore([
-    {
-      name: '仁德',
-      // type: SkillType.PU_TONG,
-      type: SkillType.JIN_YONG,
-      triggerTiming: TriggerTiming.CHU_PAI,
-      do: '',
-    },
-    {
-      name: '激将',
-      type: SkillType.BEI_DONG,
-      triggerTiming: TriggerTiming.SHI_YONG_HUO_DA_CHU,
-      do: '',
-    },
-  ])
+const SkillArea = component$<SkillAreaProps>(({skillList}) => {
+    // 测试
+    // const skillList = useStore([
+    //     {
+    //         name: '仁德',
+    //         // type: SkillType.PU_TONG,
+    //         type: SkillType.JIN_YONG,
+    //         triggerTiming: TriggerTiming.CHU_PAI,
+    //         do: '',
+    //     },
+    //     {
+    //         name: '激将',
+    //         type: SkillType.BEI_DONG,
+    //         triggerTiming: TriggerTiming.SHI_YONG_HUO_DA_CHU,
+    //         do: '',
+    //     },
+    // ])
 
-  return (
-    <div style={{
-      display: 'flex', flexDirection: 'column-reverse', alignItems: 'center',
-      width: 100, backgroundColor: 'red'
-    }}>
-      {
-        skillList.map((skill, index) => {
-          return (
-            <button key={index}
-              style={{
-                width: 87, height: 47,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundImage: `url(${import.meta.env.BASE_URL}image/skill/${skill.type}.png)`,
-              }}
-            >
-              {skill.name}
-            </button>
-          )
-        })
-      }
-    </div>
-  )
+    return (
+        <div style={{
+            display: 'flex', flexDirection: 'column-reverse', alignItems: 'center',
+            width: 100, backgroundColor: 'red'
+        }}>
+            {
+                skillList.map((skill, index) => {
+                    return (
+                        <button
+                            key={index}
+                            style={{
+                                width: 87, height: 47, backgroundSize: 'cover', backgroundPosition: 'center',
+                                backgroundImage: `url(${import.meta.env.BASE_URL}image/skill/${skill.type}.png)`,
+                            }}
+                        >
+                            {skill.name}
+                        </button>
+                    )
+                })
+            }
+        </div>
+    )
 })
 
 interface HandAreaProps { }
 
 /** 手牌区 */
 const HandArea = component$<HandAreaProps>(() => {
-  return <div style={{ display: 'flex', alignItems: 'center', flex: 1, backgroundColor: 'yellow' }}>手牌区</div>
+    const shoupaiList = useStore(shoupaiArray.slice(9, 13))
+
+    return (
+        <div
+            style={{
+                display: 'flex', alignItems: 'end', flex: 1,
+                overflowX: 'auto',
+                backgroundColor: 'yellow',
+            }}
+            onClick$={() => { alert('手牌区被点击了！！！'); shoupaiList.push(shoupaiArray[14]) }}
+        >
+            {
+                shoupaiList.map((hand, index) => {
+                    return <Hand hand={hand} key={index} />
+                })
+            }
+        </div>
+    )
 })
